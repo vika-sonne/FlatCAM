fc4e37649778235ac596ec347026ce81f41953a5
vika-sonne <vika.sonne@rambler.ru>
2024-04-16 02:15:08 +0300

    del old files

--- create_resources.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-pyrcc5 resources.qrc -o resources.py
-pyrcc5 resources_dark.qrc -o resources_dark.py
--- create_zipapp.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-# zip -vr FlatCAM.zip *.py LICENSE appCommon/*/*.py appEditors/*/*.py appGUI/*/*.py appObjects/*/*.py appParsers/*/*.py appTools/*/*.py descartes/*/*.py locale/*/*.py preprocessors/*/*.py tclCommands/*/*.py
-
-zip FlatCAM.zip -v `find -name "*.py" -print` `find "translate" -name "*.bin" -print`
